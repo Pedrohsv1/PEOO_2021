d739762d7e266b0e66d9de51b683d27d889d41ba
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2021-12-05 19:34:00 +0000

    cs files

--- Lista 05/1049.cs
+++ Lista 05/1049.cs
@@ -4,12 +4,47 @@ class URI {
 
     static void Main(string[] args) { 
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+      string A = Console.ReadLine();
+      string B = Console.ReadLine();
+      string C = Console.ReadLine();
 
+
+      if(A == "vertebrado"){
+        if(B == "ave"){
+            if(C == "onivoro"){
+              Console.WriteLine("pomba");
+            }
+            else{
+              Console.WriteLine("aguia");
+            }
+        }
+        else{
+            if(C == "onivoro"){
+              Console.WriteLine("homen");
+            }
+            else{
+              Console.WriteLine("vaca");
+            }
+        }
+      }
+      else{
+        if(B == "insetp"){
+            if(C == "hematofago"){
+              Console.WriteLine("pulga");
+            }
+            else{
+              Console.WriteLine("lagarta");
+            }
+        }
+        else{
+            if(C == "hematofago"){
+              Console.WriteLine("sanguessuga");
+            }
+            else{
+              Console.WriteLine("minhoca");
+            }
+        }
+      }
     }
 
 }
